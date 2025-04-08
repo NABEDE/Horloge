0a7af00425b875997a6292ffc95a3e253de3e467
NABEDE lidawo Jérôme Bienvenu <61563798+NABEDE@users.noreply.github.com>
2025-04-08 21:32:51 +0100

    Update main.py

--- MontreHeure/main.py
+++ MontreHeure/main.py
@@ -1,20 +1,24 @@
 from tkinter import *
 from tkinter.ttk import *
-
 from time import strftime
 
+# Configurer la fenêtre principale
 root = Tk()
+root.title("Horloge avec le langage Python")
+root.configure(background="black")
 
-root.title("Horloge avec le langage python")
+# Fonction pour mettre à jour l'heure
+def update_time():
+    current_time = strftime('%H:%M:%S %p')
+    label.config(text=current_time)
+    label.after(1000, update_time)
 
-def time():
-    string = strftime('%H:%M:%S %p')
-    label.config(text=string)
-    label.after(1000, time)
+# Créer le label pour afficher l'heure
+label = Label(root, font=("ds-digital", 80), background="black", foreground="lime")
+label.pack(anchor="center")
 
-label = Label(root, font=("ds-digital", 80), background = "black", foreground = "black")
-label.pack(anchor = "center")
+# Lancer la mise à jour de l'heure
+update_time()
 
-time()
-
-mainloop()
+# Boucle principale de l'application
+root.mainloop()
